3e77254925fa2d377b8c6a3a420be843c3cb4ca8
Kittithat Yotkhun <jkljkl4467@gmail.com>
2025-12-12 01:12:13 +0700

    edit file keep and req

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from threading import Thread
+import threading
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True
-    t.start()
+    thread = threading.Thread(target=run)
+    thread.start()
